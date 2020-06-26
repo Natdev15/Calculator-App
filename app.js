5c4413204a7cd462aee694aab38a789e3c33b156
Nateshk1529 <Nateshkumar2014@gmail.com>
2020-06-27 02:32:14 +0500

    calculator application

---
+++ app.js
@@ -0,0 +1,15 @@
+function getNumb(num){
+  
+    var result = document.getElementById("screen");
+    result.value += num;
+
+}
+
+ function clearResult(){
+    var result = document.getElementById("screen");
+    result.value = "";
+ }
+ function getResult(){
+    var result = document.getElementById("screen");
+    result.value = eval(result.value);
+ }
